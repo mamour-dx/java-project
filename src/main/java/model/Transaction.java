20fb23a35fd4880b41ce31d152bb19c77bf26f1e
Mamour Dieng <149331871+mamour-dx@users.noreply.github.com>
2025-04-05 23:30:57 +0000

    feat: add model classes for User, Account, and Transaction

---
+++ src/main/java/model/Transaction.java
@@ -0,0 +1,71 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private int transactionId;
+    private int fromAccountId;
+    private int toAccountId;
+    private double amount;
+    private LocalDateTime dateTime;
+    private String type;
+
+    public Transaction(int transactionId, int fromAccountId, int toAccountId, double amount, LocalDateTime dateTime,
+            String type) {
+        this.transactionId = transactionId;
+        this.fromAccountId = fromAccountId;
+        this.toAccountId = toAccountId;
+        this.amount = amount;
+        this.dateTime = dateTime;
+        this.type = type;
+    }
+
+    // Getters and Setters
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getFromAccountId() {
+        return fromAccountId;
+    }
+
+    public void setFromAccountId(int fromAccountId) {
+        this.fromAccountId = fromAccountId;
+    }
+
+    public int getToAccountId() {
+        return toAccountId;
+    }
+
+    public void setToAccountId(int toAccountId) {
+        this.toAccountId = toAccountId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
